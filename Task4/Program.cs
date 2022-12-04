c76365fb8ab501c5aff5a5fa7e81582adcaf40e1
Iryna <batsianiryna@gmail.com>
2022-12-04 19:24:19 +0300

    add task4 Факториал числа

---
+++ Task4/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите программу, которая на вход принимает число N и выдает произведение чисел от 1 до N. Факториал числа
+
+Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int result = 1;
+for (int i = 1; i <= n; i++)
+    result *= i; // result = result * i;
+Console.WriteLine($"Результат: {result}");

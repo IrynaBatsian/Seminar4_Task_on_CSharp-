b4998a239b66e9b4bce6d905408fb06baab46d8f
Iryna <batsianiryna@gmail.com>
2022-12-04 18:24:06 +0300

    add task 2 сумма чисел от единицы до заданного числа

---
+++ Task2/Program.cs
@@ -0,0 +1,7 @@
+﻿// Напишите программу, которая принимает на вход число (А)  и выдает сумму чисел от 1 до А
+
+Console.Clear();
+Console.Write("Введите число: ");
+double a = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine($"Результат: {(a + 1)/ 2 * a}");
+
